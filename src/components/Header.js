557c7ee1f275a7ee54d8577873047d608b8baab3
Unicorn Barf <nolan.spence7@gmail.com>
2022-09-15 16:56:38 -0700

    Implement navbar navigation functionality.  Works and changes content pages.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Navbar from './Navbar';
 
-export default function Header() {
+export default function Header({ currentPage, pageChange }) {
 
     return (
         <div>
             <h1>Nolan Spence</h1>
-            <Navbar/>
+            <Navbar currentPage={currentPage} pageChange={pageChange}/>
         </div>
     )
 };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,35 @@
 import React from 'react';
 
-export default function Navbar() {
+export default function Navbar({ currentPage, pageChange }) {
 
 
     return (
         <div>
             <nav>
-                <a href="#about">ABOUT</a>
-                <a href="#projects">PROJECTS</a>
-                <a href="#contact">CONTACT</a>
-                <a href="#resume">RESUME</a>
+                <a
+                    href="#about"
+                    onClick={() => pageChange('about')}
+                >
+                    ABOUT
+                </a>
+                <a
+                    href="#projects"
+                    onClick={() => pageChange('projects')}
+                >
+                    PROJECTS
+                </a>
+                <a
+                    href="#contact"
+                    onClick={() => pageChange('contact')}
+                >
+                    CONTACT
+                </a>
+                <a
+                    href="#resume"
+                    onClick={() => pageChange('resume')}
+                >
+                    RESUME
+                </a>
             </nav>
         </div>
     );
