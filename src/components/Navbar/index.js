c2a48af78293c63f65151ee65a2a19d451a92a0e
Unicorn Barf <nolan.spence7@gmail.com>
2022-09-20 00:08:00 -0700

    Changed component folder structure to component directory with index.js and style.css

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './style.css';
 
 export default function Navbar({ currentPage, pageChange }) {
 
@@ -9,24 +10,40 @@ export default function Navbar({ currentPage, pageChange }) {
                 <a
                     href="#about"
                     onClick={() => pageChange('about')}
+                    className={currentPage === 'about'
+                        ? 'nav-link current'
+                        : 'nav-link'
+                    }
                 >
                     ABOUT
                 </a>
                 <a
                     href="#projects"
                     onClick={() => pageChange('projects')}
+                    className={currentPage === 'projects'
+                        ? 'nav-link current'
+                        : 'nav-link'
+                    }
                 >
                     PROJECTS
                 </a>
                 <a
                     href="#contact"
                     onClick={() => pageChange('contact')}
+                    className={currentPage === 'contact'
+                        ? 'nav-link current'
+                        : 'nav-link'
+                    }
                 >
                     CONTACT
                 </a>
                 <a
                     href="#resume"
                     onClick={() => pageChange('resume')}
+                    className={currentPage === 'resume'
+                        ? 'nav-link current'
+                        : 'nav-link'
+                    }
                 >
                     RESUME
                 </a>
